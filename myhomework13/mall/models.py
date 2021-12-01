cd18a3d574914a6e329a986e5b6ab37085abd1c8
Jina Kim <fghjiurt@naver.com>
2021-12-01 23:34:50 +0900

    mall/shop description 수정

--- myhomework13/mall/models.py
+++ myhomework13/mall/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Shop(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TimeField(blank=True)
+    description = models.TextField(blank=True)
     telephone = models.CharField(max_length=13)
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)

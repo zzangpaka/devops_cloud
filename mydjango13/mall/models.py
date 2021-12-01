188e2be1537fb671789844f9ffee6c6622301abb
Jina Kim <fghjiurt@naver.com>
2021-12-01 17:33:00 +0900

    21_12_01 django 세번째 수업

---
+++ mydjango13/mall/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Shop(models.Model):
+    # id = models.BigAutoField(primary_key=True) PK 지정
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=13)
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # 관련 쿼리셋에서 order_by를 지정하지 않을 때
+        # 적용될 디폴트 정렬
+        ordering = ['-id']
+        

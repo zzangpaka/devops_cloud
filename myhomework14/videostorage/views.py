8307002c1b00a5a4442866e6aab0c2f564ad5870
Jina Kim <fghjiurt@naver.com>
2021-12-02 23:22:04 +0900

    조회수 적용...?

--- myhomework14/videostorage/views.py
+++ myhomework14/videostorage/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.db.models import F
 
 from videostorage.models import Video
-
 
 index = ListView.as_view(model=Video, template_name="videostorage/index.html")
 
 
 def video_detail(request: HttpRequest, pk: int) -> HttpResponse:
     video = Video.objects.get(pk=pk)
+    Video.objects.filter(pk=pk).update(view_count=F('view_count') + 1)
     return render(
         request,
         "videostorage/video_detail.html",

9ee7a2c75fa27609040406ec7b78083f970bb5dc
Jina Kim <fghjiurt@naver.com>
2021-12-02 10:41:56 +0900

    mydjango14 프로젝트 생성 및 초기 구성

---
+++ mydjango14/mydjango14/urls.py
@@ -0,0 +1,18 @@
+import debug_toolbar.toolbar
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls))
+    ]

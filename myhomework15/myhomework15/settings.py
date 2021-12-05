59da8960e5ab09b160578661ca93a485763f7b86
Jina Kim <fghjiurt@naver.com>
2021-12-06 00:37:52 +0900

    debug-toolbar / media 세팅

--- myhomework15/myhomework15/settings.py
+++ myhomework15/myhomework15/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mypet',
 ]
+
+if DEBUG:
+    INSTALLED_APPS += [
+        'debug_toolbar'
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+             ] + MIDDLEWARE
 
 ROOT_URLCONF = 'myhomework15.urls'
 
@@ -119,7 +129,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ['127.0.0.1']

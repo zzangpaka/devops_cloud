74e3cf4f17f9cb5ecc8e1663d50a174ae44ab248
Jina Kim <fghjiurt@naver.com>
2021-12-06 15:36:31 +0900

    blog app 생성 및 등록

--- mydjango16/mydjango16/settings.py
+++ mydjango16/mydjango16/settings.py
@@ -33,12 +33,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
+    # local apps
+    'blog',
 ]
 
 if DEBUG:
@@ -130,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

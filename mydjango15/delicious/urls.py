ae4467e66afe7890be66f655c12ba63d0b518ccb
Jina Kim <fghjiurt@naver.com>
2021-12-06 15:17:27 +0900

    delicious 앱에 대한 URL Reverse 적용

---
+++ mydjango15/delicious/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from delicious.views import shop_list, shop_detail, shop_new_1, shop_new
+
+app_name = "delicious" # namespace
+
+urlpatterns = [
+    path('', shop_list, name='shop_list'),
+    path('<int:pk>/', shop_detail, name='shop_detail'),
+    path('new1/', shop_new_1, name='shop_new_1'),
+    path('new/', shop_new, name='shop_new'),
+]
--- mydjango15/mydjango15/urls.py
+++ mydjango15/mydjango15/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from delicious.views import shop_list, shop_detail, shop_new_1, shop_new
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('delicious/', shop_list),
-    path('delicious/<int:pk>/', shop_detail),
-    path('delicious/new1/', shop_new_1),
-    path('delicious/new/', shop_new),
+    path('delicious/', include('delicious.urls')),
 ]

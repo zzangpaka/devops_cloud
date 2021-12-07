12966578327035e3b51ac9826c4895226b2770da
Jina Kim <fghjiurt@naver.com>
2021-12-07 21:40:56 +0900

    admin 추가 및 views 구현

--- practice03/book/admin.py
+++ practice03/book/admin.py
@@ -3,19 +3,20 @@ from book.models import Profile, Book, Comment, Tag
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "updated_at"]
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "genre", "title", "author", "updated_at"]
+    list_display_links = ["title"]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["message", "name", "updated_at"]
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "updated_at"]
--- practice03/book/views.py
+++ practice03/book/views.py
@@ -1,3 +1,42 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from book.models import Book
+
+
+def tag_detail(request: HttpRequest, tag_name: str) -> HttpResponse:
+    qs = Book.objects.all()
+    qs = qs.filter(tag_set__name=tag_name)
+    return render(request, "book/tag_detail.html", {
+        "tag_name": tag_name,
+        "book_list": qs,
+    })
+
+
+def profile(request: HttpRequest) -> HttpResponse:
+    return render(request, "book/profile.html")
+
+
+def book_list(request: HttpRequest) -> HttpResponse:
+    qs = Book.objects.all()
+
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(title__icontains=query)
+
+    context_data = {
+        "book_list": qs,
+    }
+
+    return render(request, "book/book_list.html", context_data)
+
+
+def book_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    book = Book.objects.get(pk=pk)
+    comment_list = book.comment_set.all()
+    tag_list = book.tag_set.all()
+    return render(request, "book/book_detail.html", {
+        "book": book,
+        "comment_list": comment_list,
+        "tag_list": tag_list,
+    })

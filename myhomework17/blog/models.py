c7ed5f15e4c9e25f94553aa0ee58c74646b54516
Jina Kim <fghjiurt@naver.com>
2021-12-07 22:52:47 +0900

    model 설정

--- myhomework17/blog/models.py
+++ myhomework17/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(TimestampedModel):
+    genre = models.CharField(max_length=2)
+    author = models.CharField(max_length=20, db_index=True)
+    title = models.CharField(max_length=100, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to="blog/post/%y/%m/%d")
+    tag_set = models.ManyToManyField("Tag", blank=True)
+
+
+class Comment(TimestampedModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    message = models.TextField()
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, db_index=True)

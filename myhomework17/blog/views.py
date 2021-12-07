1bbfd59df64bd9ae1186b11ff702b585886952ad
Jina Kim <fghjiurt@naver.com>
2021-12-07 23:56:34 +0900

    comment, tag 설정

--- myhomework17/blog/views.py
+++ myhomework17/blog/views.py
@@ -11,13 +11,19 @@ def post_list(request: HttpRequest) -> HttpResponse:
     if query:
         qs = qs.filter(title__icontains=query)
 
-    return render(request, "blog/post_list.html", {
+    context_data = {
         "post_list": qs,
-    })
+    }
+
+    return render(request, "blog/post_list.html", context_data)
 
 
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     post = Post.objects.get(pk=pk)
+    comment_list = post.comment_set.all()
+    tag_list = post.tag_set.all()
     return render(request, "blog/post_detail.html", {
         "post": post,
+        "comment_list": comment_list,
+        "tag_list": tag_list,
     })

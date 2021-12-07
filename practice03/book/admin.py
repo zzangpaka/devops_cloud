1a2fce6c29e7962a603573286d29c3e25bc856a1
Jina Kim <fghjiurt@naver.com>
2021-12-07 18:50:34 +0900

    admin 설정

--- practice03/book/admin.py
+++ practice03/book/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from book.models import Profile, Book, Comment, Tag
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass

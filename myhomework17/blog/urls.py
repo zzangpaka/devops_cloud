b9180d40a438154c04d98e6411d2019ddadb1378
Jina Kim <fghjiurt@naver.com>
2021-12-08 00:03:05 +0900

    tag_detail 뷰 구현

--- myhomework17/blog/urls.py
+++ myhomework17/blog/urls.py
@@ -8,4 +8,5 @@ app_name = "blog"
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("<int:pk>/", views.post_detail, name="post.detail"),
+    path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
 ]

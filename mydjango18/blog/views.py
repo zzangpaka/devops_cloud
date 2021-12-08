4df3848ab6776e4a05337288b169918b15a8110d
Jina Kim <fghjiurt@naver.com>
2021-12-08 16:16:23 +0900

    profile views 생성 및 layout, profile html 설정

--- mydjango18/blog/views.py
+++ mydjango18/blog/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Profile
+
+
+def profile(request: HttpRequest) -> HttpResponse:
+    qs = Profile.objects.all()
+    return render(request, "blog/profile.html", {
+        "profile": qs,
+    })

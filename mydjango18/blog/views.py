7117a6e44acdef3c44e815d95ab465bd60683e7a
Jina Kim <fghjiurt@naver.com>
2021-12-08 22:36:32 +0900

    form을 이용한 검색창 및 post_detail 설정

--- mydjango18/blog/views.py
+++ mydjango18/blog/views.py
@@ -13,7 +13,18 @@ def profile(request: HttpRequest) -> HttpResponse:
 
 def post_list(request: HttpRequest) -> HttpResponse:
     qs = Post.objects.all()
+
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(title__icontains=query)
+
     return render(request, "blog/post_list.html", {
         "post_list": qs,
     })
 
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)
+    return render(request, "blog/post_detail.html", {
+        "post": post,
+    })

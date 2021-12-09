f954dfe6c0dfd12153a711b03babae4aec235ca0
Jina Kim <fghjiurt@naver.com>
2021-12-09 15:08:36 +0900

    Post 모델의 title 필드에 커스텀 validator 설정

--- mydjango19/diary/models.py
+++ mydjango19/diary/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class TimestampedModel(models.Model):
@@ -11,7 +12,14 @@ class TimestampedModel(models.Model):
 
 class Post(TimestampedModel):
     author_name = models.CharField(max_length=20)
-    title = models.CharField(max_length=200, db_index=True)
+    title = models.CharField(
+        max_length=200,
+        db_index=True,
+        validators=[
+            MinLengthValidator(3),
+            # MaxLengthValidator(),
+        ],
+    )
     content = models.TextField()
     photo = models.ImageField(upload_to="diary/post/%y/%m/%d")
     tag_set = models.ManyToManyField("Tag", blank=True)

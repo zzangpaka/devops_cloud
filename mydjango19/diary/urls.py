2ba61b85c81955f01acaf055f48e24ef7479c64a
Jina Kim <fghjiurt@naver.com>
2021-12-09 13:39:39 +0900

    django form 실습

--- mydjango19/diary/urls.py
+++ mydjango19/diary/urls.py
@@ -8,5 +8,6 @@ app_name = "diary"
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("<int:pk>/", views.post_detail, name="post_detail"),
+    path("new/", views.post_new, name="post_new"),
     path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
 ]
--- mydjango19/diary/views.py
+++ mydjango19/diary/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from diary.forms import PostForm
 from diary.models import Post
 
 
@@ -44,3 +45,29 @@ def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
             "tag_list": tag_list,
         },
     )
+
+
+def post_new(request: HttpRequest) -> HttpResponse:
+    # print("request.method :", request.method)
+    # print("request.GET :", request.GET)
+    # print("request.POST :", request.POST)
+    # print("request.FILES :", request.FILES)
+
+    # 입력 서식을 보여줌
+    if request.method == "GET":
+        form = PostForm()
+    # 서식 입력값을 전달받아 유효성 검사 실행
+    # -> 에러 상황에서 에러메세지 출력
+    # -> 유효성 검사 통과 시 입력값을 출력하고 post_list로 이동
+    else:
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("유효성 검사에 통과했습니다. :", form.cleaned_data)
+            form.save() # ModelForm에서만 지원
+            return redirect("diary:post_list")
+        else:
+            pass
+
+    return render(request, "diary/post_form.html", {
+        "form": form,
+    })

72841914d7e8c45c30f9c869242500caed6985bd
Jina Kim <fghjiurt@naver.com>
2021-12-13 22:19:50 +0900

    url 설정 및 root 설정

--- myhomework21_1/shop/urls.py
+++ myhomework21_1/shop/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path
+
 from shop import views
 
 
 app_name = 'shop'
 
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.shop_list, name="shop_list"),
+    path("<int:pk>/", views.shop_detail, name="shop_detail"),
+    path("new/", views.shop_new, name="shop_new"),
+    path("<int:pk>/edit/", views.shop_edit, name="shop_edit"),
+    path("tags/<str:tag_name>/", views.tag_detail, name="tag_detail"),
+]

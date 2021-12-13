fbcc60fec0770f1fbe973ee0cd6e97ef534508d5
Jina Kim <fghjiurt@naver.com>
2021-12-13 22:54:16 +0900

    Tag 저장 로직 ShopForm에 설정

--- myhomework21_1/shop/forms.py
+++ myhomework21_1/shop/forms.py
@@ -1,9 +1,24 @@
 from django import forms
-from shop.models import Shop
+from shop.models import Shop, Tag
 
 
 class ShopForm(forms.ModelForm):
     tags = forms.CharField()
+
+    def save(self):
+        saved_post = super().save()
+
+        tag_list = []
+        tags = self.cleaned_data.get("tags", "")
+        for word in tags.split(","):
+            tag_name = word.strip()
+            tag, __ = Tag.objects.get_or_create(name=tag_name)
+            tag_list.append(tag)
+
+        saved_post.tag_set.clear()
+        saved_post.tag_set.add(*tag_list)
+
+        return saved_post
 
     class Meta:
         model = Shop
@@ -11,5 +26,6 @@ class ShopForm(forms.ModelForm):
             "category",
             "name",
             "telephone",
+            "photo",
             "description",
-        ]
+]

1fd2cc3724de448612d31573ea3d6598121387fe
Jina Kim <fghjiurt@naver.com>
2021-12-13 22:55:11 +0900

    필드 초기값 설정

--- myhomework21_1/shop/forms.py
+++ myhomework21_1/shop/forms.py
@@ -5,20 +5,24 @@ from shop.models import Shop, Tag
 class ShopForm(forms.ModelForm):
     tags = forms.CharField()
 
-    def save(self):
-        saved_post = super().save()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            tag_qs = self.instance.tag_set.all()
+            tags = ", ".join([tag.name for tag in tag_qs])
+            self.fields["tags"].initial = tags
 
+    def save(self):
+        saved_shop = super().save()
         tag_list = []
         tags = self.cleaned_data.get("tags", "")
         for word in tags.split(","):
             tag_name = word.strip()
             tag, __ = Tag.objects.get_or_create(name=tag_name)
             tag_list.append(tag)
-
-        saved_post.tag_set.clear()
-        saved_post.tag_set.add(*tag_list)
-
-        return saved_post
+        saved_shop.tag_set.clear()
+        saved_shop.tag_set.add(*tag_list)
+        return saved_shop
 
     class Meta:
         model = Shop
@@ -28,4 +32,4 @@ class ShopForm(forms.ModelForm):
             "telephone",
             "photo",
             "description",
-]
+        ]

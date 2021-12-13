a11c174d2a2584f8d59b856a27a6e355aead1656
Jina Kim <fghjiurt@naver.com>
2021-12-13 21:13:01 +0900

    shop model 설정

--- myhomework21_1/shop/models.py
+++ myhomework21_1/shop/models.py
@@ -1,3 +1,35 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Shop(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField()
+    telephone = models.CharField(max_length=14,
+                                 validators=[
+                                     RegexValidator(r"^\d{3}-?\d{4}-?\d{4}$", message="전화번호를 입력해주세요.")
+                                 ],
+                                 help_text="예) 042-1234-1234")
+    photo = models.ImageField(upload_to="shop/photo/%y/%m/%d")
+    tag_set = models.ManyToManyField('tag', blank=True)
+
+
+    def __str__(self) -> str:
+        return f"[{self.pk}] {self.name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+    def __str__(self) -> str:
+        return self.name

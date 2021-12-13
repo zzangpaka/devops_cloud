80c27f204d8f955f954c518d8739b85b8e6d58dd
Jina Kim <fghjiurt@naver.com>
2021-12-13 22:56:42 +0900

    ShopAdmin에 ShopForm을 지정

--- myhomework21_1/shop/admin.py
+++ myhomework21_1/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from shop.forms import ShopForm
 from shop.models import Category, Shop, Tag
 
 
@@ -10,6 +11,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
+    form = ShopForm
+
     list_display = ['id', 'name', 'telephone']
     list_display_links = ['name']
 

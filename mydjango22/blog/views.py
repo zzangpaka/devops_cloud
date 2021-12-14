78957264dac6c6f07f60958c2038e5d553463826
Jina Kim <fghjiurt@naver.com>
2021-12-14 11:25:28 +0900

    post_list 기본구현

--- mydjango22/blog/views.py
+++ mydjango22/blog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post
+
+
+def Post_list(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+
+    return render(request, "blog/post_list.html", {
+        "post_list": qs,
+    })
+
+

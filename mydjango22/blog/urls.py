3428622904b340e52c2037d0360347487c7982e6
Jina Kim <fghjiurt@naver.com>
2021-12-14 11:36:37 +0900

    layout, post_list html 설정 및 urls 설정

---
+++ mydjango22/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from blog import views
+
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    path('', views.post_list, name="post_list"),
+]
--- mydjango22/mydjango22/urls.py
+++ mydjango22/mydjango22/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL,

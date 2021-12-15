21ae381cf1f9ae0c7fa4bc7665c70672aaf0bd17
Jina Kim <fghjiurt@naver.com>
2021-12-15 14:44:34 +0900

    PostCreateView/PostUpdateView에 post_detail로의 success_url 설정

--- mydjango23/blog/views/cbv.py
+++ mydjango23/blog/views/cbv.py
@@ -1,6 +1,7 @@
 
 # Class Based View(CBV)
 # - 뷰 함수를 만들어주는 클래스
+from django.shortcuts import resolve_url
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -18,23 +19,38 @@ post_detail = DetailView.as_view(
 )
 
 
-post_new = CreateView.as_view(
-    model=Post,
-    form_class=PostForm,
-    success_url=reverse_lazy("blog:post_list"),
-)
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    # success_url = reverse_lazy("blog:post_list")
+
+    def get_success_url(self):
+        # self.object # 저장된 모델 인스턴스
+        post_pk = self.object.pk
+        return reverse("blog:post_detail", args=[post_pk]) # 문자열(리턴값) -> 성능 젤 떨어짐
+        # return resolve_url("blog:post_detail", post_pk) # 문자열(리턴값)
+        # return redirect("blog:post_detail", post_pk) # HttpResponse(리턴값)
+        # {% url "blog:post_detail" post_pk %} # 문자열(리턴값)
 
 
-post_edit = UpdateView.as_view(
-    model=Post,
-    form_class=PostForm,
+post_new = PostCreateView.as_view()
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
     # TODO: 가변적으로 URL을 지정할 수 없다.
     # TODO: URL Reverse가 미적용
     # success_url='/blog/'
     # success_url="blog:post_list", # URL Reverse 미지원
     # success_url=reverse("blog:post_list"),
-    success_url=reverse_lazy("blog:post_list"),
-)
+
+    def get_success_url(self):
+        post_pk = self.object.pk
+        return reverse("blog:post_detail", args=[post_pk])
+
+
+post_edit = PostUpdateView.as_view()
 
 
 post_delete = DeleteView.as_view(

f3c68e37acb94ba707a739bf7a3e1f345a43bfa2
Jina Kim <fghjiurt@naver.com>
2021-12-15 10:38:23 +0900

    root 기초 설정(url 사용)

--- mydjango23/mydjango23/urls.py
+++ mydjango23/mydjango23/urls.py
@@ -17,10 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    # 아래의 url 설정은 blog 기능 구현 후에 pattern_name으로 변경 예정
+    path('', RedirectView.as_view(url='/blog/'), name="root"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,

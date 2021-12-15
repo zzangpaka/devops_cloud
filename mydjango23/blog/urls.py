944163c52f7d1b42104cbd17afc0ba4a97de1fb4
Jina Kim <fghjiurt@naver.com>
2021-12-15 14:03:20 +0900

    blog/views.py를 blog/views/fbv.py로 이동

--- mydjango23/blog/urls.py
+++ mydjango23/blog/urls.py
@@ -1,1 +1,16 @@
-urlpatterns = []
+from django.urls import path
+
+from blog import views
+from blog.views import fbv
+
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    path('', fbv.post_list, name='post_list'),
+    path('<int:pk>/', fbv.post_detail, name='post_detail'),
+    path('new/', fbv.post_new, name='post_new'),
+    path('<int:pk>/edit/', fbv.post_edit, name='post_edit'),
+    path('<int:pk>/delete/', fbv.post_delete, name='post_delete'),
+]

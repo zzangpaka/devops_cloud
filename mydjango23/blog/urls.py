b048c78131fd8ed6bb7e1e707dc155b0b15a189f
Jina Kim <fghjiurt@naver.com>
2021-12-15 14:26:21 +0900

    fbv 내역대로 cbv를 구현

--- mydjango23/blog/urls.py
+++ mydjango23/blog/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 
 from blog import views
-from blog.views import fbv
+from blog.views import cbv as views
+# from blog.views import fbv as views
 
 
 app_name = 'blog'
 
 
 urlpatterns = [
-    path('', fbv.post_list, name='post_list'),
-    path('<int:pk>/', fbv.post_detail, name='post_detail'),
-    path('new/', fbv.post_new, name='post_new'),
-    path('<int:pk>/edit/', fbv.post_edit, name='post_edit'),
-    path('<int:pk>/delete/', fbv.post_delete, name='post_delete'),
+    path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('new/', views.post_new, name='post_new'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]

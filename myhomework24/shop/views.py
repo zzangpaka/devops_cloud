85a714a8e4cc35cbdc7ff76b2cff30bcdc59a20d
Jina Kim <fghjiurt@naver.com>
2021-12-16 18:44:18 +0900

    shop에 관한 클래스 기반 뷰 간단구현

--- myhomework24/shop/views.py
+++ myhomework24/shop/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from shop.forms import ShopForm
+from shop.models import Shop
 
-# Create your views here.
+shop_list = ListView.as_view(
+    model = Shop,
+)
+
+
+shop_detail = DetailView.as_view(
+    model = Shop,
+)
+
+
+shop_new = CreateView.as_view(
+    model = Shop,
+    form_class = ShopForm,
+)
+
+
+shop_edit = UpdateView.as_view(
+    model = Shop,
+    form_class = ShopForm
+)
+
+
+shop_delete = DeleteView.as_view(
+    model = Shop,
+    success_url=reverse_lazy("shop:shop_list"),
+)
+
+
+# resell_list
+#
+#
+# resell_detail
+#
+#
+# resell_new
+#
+#
+# resell_edit
+#
+#
+# resell_delete

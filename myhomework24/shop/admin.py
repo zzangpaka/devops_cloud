ad312eb68b82c3c7cf9c6e986292b53b10e47cbb
Jina Kim <fghjiurt@naver.com>
2021-12-16 18:27:08 +0900

    layout 생성 및 shop 모델 / admin 설정과 마이그레이션

--- myhomework24/shop/admin.py
+++ myhomework24/shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Category, Shop, Tag, Resell
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ["category", "title", "author_name"]
+    list_display_links = ["title"]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+@admin.register(Resell)
+class ResellAdmin(admin.ModelAdmin):
+    list_display = ["category", "title", "user"]
+    list_display_links = ["title"]
--- myhomework24/shop/models.py
+++ myhomework24/shop/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Shop(TimestampedModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=100, db_index=True)
+    title = models.CharField(max_length=100, db_index=True)
+    photo = models.ImageField(upload_to="shop/photo/%y/%m/%d")
+    description = models.TextField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self) -> str:
+        return f"[{self.pk}] {self.title}"
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Resell(TimestampedModel):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    user = models.CharField(max_length=20, db_index=True)
+    parcel = models.BooleanField(null=True, verbose_name="택배비포함")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, db_index=True)
+    photo = models.ImageField(upload_to="shop/photo/%y/%m/%d", blank=True)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["-id"]

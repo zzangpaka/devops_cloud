8f3c73846144b2870c8d902d61d7a0d6ed793500
Jina Kim <fghjiurt@naver.com>
2021-12-17 14:32:44 +0900

    shop_list 구현

--- mydjango25/shop/urls.py
+++ mydjango25/shop/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
+
+from shop import views
 
 
 app_name = "shop"
 
-urlpatterns = []
+
+urlpatterns = [
+    path("", views.shop_list, name="shop_list"),
+]
--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from shop.models import Shop, Category
 
 
+class ShopListView(ListView):
+    model = Shop
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["category_list"] = Category.objects.all()
+        return context_data
+
+
+shop_list = ShopListView.as_view()

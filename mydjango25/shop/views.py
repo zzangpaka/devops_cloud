1b2c26963ed5c22ef674ba040c66a63644027351
Jina Kim <fghjiurt@naver.com>
2021-12-17 16:46:04 +0900

    페이징 등 수업 마무리

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, resolve_url
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from shop.forms import ReviewForm
+from shop.mixins import ReviewUserCheckMixin
 from shop.models import Shop, Category, Review
 
 
 class ShopListView(ListView):
     model = Shop
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
@@ -28,8 +30,8 @@ shop_detail = DetailView.as_view(
 class ReviewCreateView(LoginRequiredMixin, CreateView):
     model = Review
     form_class = ReviewForm
-    # TODO: shop detail로 이동
-    success_url = reverse_lazy("shop:shop_list")
+    # # TODO: shop detail로 이동
+    # success_url = reverse_lazy("shop:shop_list")
 
     # 유효성 검사에 통과한 후에 호출 ...
     def form_valid(self, form) -> HttpResponse:
@@ -47,3 +49,17 @@ class ReviewCreateView(LoginRequiredMixin, CreateView):
 
 
 review_new = ReviewCreateView.as_view()
+
+
+class ReviewUpdateView(LoginRequiredMixin, ReviewUserCheckMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    # FIXME: shop detail로 보내기
+    # success_url = reverse_lazy("shop:shop_list")
+
+    def get_success_url(self) -> str:
+        review = self.object
+        return resolve_url(review.shop)
+
+
+review_edit = ReviewUpdateView.as_view()

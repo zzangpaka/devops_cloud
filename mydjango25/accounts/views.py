c7ea512b25e44ffed8bf5ad8abadd8a3a4773085
Jina Kim <fghjiurt@naver.com>
2021-12-17 13:20:39 +0900

    프로필 / 로그아웃 만들고 최상위 html에 네비게이션바 생성

--- mydjango25/accounts/views.py
+++ mydjango25/accounts/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 signup = CreateView.as_view(
     form_class=UserCreationForm,
@@ -14,10 +15,12 @@ login = LoginView.as_view(
     template_name="accounts/login_form.html"
 )
 
+# TODO: 커스텀 CBV를 만든다면, LoginRequiredMixin을 상속받도록 할 수 있다.
+profile = login_required(TemplateView.as_view(
+    template_name="accounts/profile.html"
+))
 
-def profile(request):
-    pass
 
-
-def logout(request):
-    pass
+logout = LogoutView.as_view(
+    next_page="root"
+)

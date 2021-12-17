c64c2361e588ee5696cccaec5b7abb1942953f50
Jina Kim <fghjiurt@naver.com>
2021-12-17 11:25:48 +0900

    accounts 회원가입/로그인 기능 설정

--- mydjango25/accounts/views.py
+++ mydjango25/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+signup = CreateView.as_view(
+    form_class=UserCreationForm,
+    success_url=reverse_lazy("accounts:login"),
+    template_name="accounts/signup_form.html"
+)
+
+
+login = LoginView.as_view(
+    template_name="accounts/login_form.html"
+)
+
+
+def profile(request):
+    pass
+
+
+def logout(request):
+    pass
--- mydjango25/mydjango25/urls.py
+++ mydjango25/mydjango25/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', TemplateView.as_view(template_name='root.html'), name='root'),
 ]
 

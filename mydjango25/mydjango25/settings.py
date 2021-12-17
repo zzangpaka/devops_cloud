70578d8c69a1b38c339916b0dbec11598b33b03c
Jina Kim <fghjiurt@naver.com>
2021-12-17 10:34:01 +0900

    django-bootstrap-v5 세팅

--- mydjango25/mydjango25/settings.py
+++ mydjango25/mydjango25/settings.py
@@ -45,6 +45,7 @@ if DEBUG:
     ]
 
 MIDDLEWARE = [
+    # django apps
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # third apps
+    'bootstrap5',
+    # local apps
 ]
 
 if DEBUG:

c73987ef84de88b967909bcf776a9ef401a673c3
Jina Kim <fghjiurt@naver.com>
2021-12-22 14:23:55 +0900

    방탄소년단 좋아요의 총 합(식 수정 + sum 생성)

--- myproj27/main10.js
+++ myproj27/main10.js
@@ -1,14 +1,26 @@
 const { melon_data: song_array } = require("./melon_data");
 
+Array.prototype.sum = function () {
+    return this.reduce((acc, element) => {
+        return acc + element;
+    }, 0);
+};
+
+
 console.log(``)
 console.log(`---->>>> TODO: #10 방탄소년단의 좋아요의 총 합은? <<<<----`)
 console.log(``)
 
 // Array의 filter와 reduce를 활용해주세요.
 
-filter_array = song_array.filter(song => song.artist === "방탄소년단");
 
-const result = filter_array.reduce((acc, cur) => { return acc += cur.like; }, 0);
+const bts_result = song_array
+    .filter(
+        ({ artist }) => artist === "방탄소년단"
+    )
+    // .reduce((acc, { like }) => acc + like, 0);
+    .map(({ like }) => like)
+    .sum();
 
 
-console.log(`총 합 : [${result}]`);
+console.log("총 합 :", `[${bts_result}]`)

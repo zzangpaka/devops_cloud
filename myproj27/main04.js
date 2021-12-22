ce223698c8f0d4287c4c9e33c5a9d022356c5e1d
Jina Kim <fghjiurt@naver.com>
2021-12-22 12:39:21 +0900

    좋아요수 200,000 이상인 곡명만 출력(식 수정)

--- myproj27/main04.js
+++ myproj27/main04.js
@@ -7,12 +7,10 @@ console.log(``)
 // Array의 filter 활용
 // 출력포맷 : `[좋아요수] 곡명 가수명`
 
-filter_array = song_array.filter(song => song.like >= 200000);
 
-filter_array.sort(function (song1, song2) {
-    return song1.like - song2.like;
-});
+const filtered_song_array = song_array
+    .filter(song => song.like >= 200_000)
 
-for (const song of filter_array) {
+for (const song of filtered_song_array) {
     console.log(`[${song.like}]`, song.title, song.artist);
 }

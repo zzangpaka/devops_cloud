80aff8cf8afb747f0b37374c313b05d02813ed4d
Jina Kim <fghjiurt@naver.com>
2021-12-22 18:36:14 +0900

    방탄소년단의 곡 중에 좋아요 수가 가장 큰 곡명 (수업)

--- myproj27/main13.js
+++ myproj27/main13.js
@@ -1,11 +1,47 @@
 const { melon_data: song_array } = require("./melon_data");
 
+Array.prototype.max = function (key_fn) {
+    return this.reduce((acc, song) => {
+        return key_fn(acc) < key_fn(song) ? song : acc;
+    });
+};
+
+Array.prototype.min = function (key_fn) {
+    return this.reduce((acc, song) => {
+        return key_fn(acc) > key_fn(song) ? song : acc;
+    });
+};
+
+
 console.log(``)
 console.log(`---->>>> TODO: #13 방탄소년단의 곡 중에 좋아요 수가 가장 큰 곡명은? <<<<----`)
 console.log(``)
 
+
 // Array의 filter와 reduce를 활용해주세요
 
-const BTS = song_array.filter(song => song.artist === "방탄소년단")
 
-console.log(BTS)
+const top_song = song_array
+    .filter(({ artist }) => artist === "방탄소년단")
+    .max(song => song.like);
+
+
+console.log(top_song)
+
+
+const top_rank = song_array
+    .filter(({ artist }) => artist === "방탄소년단")
+    .min(song => song.rank);
+
+
+console.log(top_rank)
+
+
+// const top_song = song_array
+//     .filter(({ artist }) => artist === "방탄소년단")
+//     .reduce((acc, song) => {
+//         return acc.like < song.like ? song : acc;
+//     });
+
+
+// console.log(top_song)

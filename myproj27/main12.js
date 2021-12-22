5896a5ff9bb4790c92d731b40c121beba2cdd2e9
Jina Kim <fghjiurt@naver.com>
2021-12-22 18:36:00 +0900

    2곡 이상 랭크된 가수는 총 몇 팀인가 (수업)

--- myproj27/main12.js
+++ myproj27/main12.js
@@ -6,18 +6,56 @@ console.log(``)
 
 // reduce, Set
 
-const singer = song_array.map(
-    (song) => (song.artist));
 
-const popular = singer.reduce(function (allsinger, singer) {
-    if (singer in allsinger) {
-        allsinger[singer]++;
-    }
-    else {
-        allsinger[singer] = 1;
-    }
-    return allsinger;
-}, {});
+const artist_count_object = song_array
+    .reduce((acc, { artist }) => {
+        // if(!acc[artist]) acc[artist] = 0;
+        acc[artist] ||= 0;
+        // i = i || 1
+        // i ||= 1
+        acc[artist] += 1;
+        return acc;
+    }, {});
+
+const artist_count = Object.values(artist_count_object)
+    .filter(number => number >= 2)
+    .length;
 
 
-console.log(popular)
+console.log(`총 ${artist_count}팀 입니다.`)
+
+
+// python version
+//
+// if artist not in acc:
+//      acc[artist] = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const singer = song_array.map(
+//     (song) => (song.artist));
+
+// const popular = singer.reduce(function (allsinger, singer) {
+//     if (singer in allsinger) {
+//         allsinger[singer]++;
+//     }
+//     else {
+//         allsinger[singer] = 1;
+//     }
+//     return allsinger;
+// }, {});
+
+
+// console.log(popular)

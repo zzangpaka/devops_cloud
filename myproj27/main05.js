96464a07d56e2db947ec70fff61549d2bb4e2590
Jina Kim <fghjiurt@naver.com>
2021-12-22 12:39:36 +0900

    좋아요수 200,000이상인 곡에 대해 곡명 오름차순 정렬(식 수정)

--- myproj27/main05.js
+++ myproj27/main05.js
@@ -7,22 +7,55 @@ console.log(``)
 // Array의 filter와 sort를 연계
 // 출력포맷 : `[좋아요수] 곡명 가수명`
 
-filter_array = song_array.filter(song => song.like >= 200000);
 
-filter_array.sort(function (song1, song2) {
-    const titleA = song1.title.toUpperCase(); // ignore upper and lowercase
-    const titleB = song2.title.toUpperCase(); // ignore upper and lowercase
-    if (titleA < titleB) {
-        return -1;
+function compare_string_for_sort(string1, string2, is_ascending = true) {
+    if (string1 < string2) {
+        // 3항 연산자 (c, c++, java 등)
+        return is_ascending ? -1 : 1;
     }
-    if (titleA > titleB) {
-        return 1;
+    else if (string1 > string2) {
+        return is_ascending ? 1 : -1;
     }
-    if (titleA === titleB) {
+    else {
         return 0;
     }
-});
-
-for (const song of filter_array) {
-    console.log(`[${song.like}]`, song.title, song.artist);
 }
+
+// 대소문자 구분 안하는 것...?
+
+const new_song_array = song_array
+    .filter(({ like }) => like >= 200_000)
+    .sort(
+        (song1, song2) => {
+            return compare_string_for_sort(song1.title, song2.title, true)
+        }
+    );
+
+
+for (const { like, title, artist } of new_song_array) {
+    console.log(`[${like}]`, title, artist);
+}
+
+
+// 오름차순
+// song1이 song2보다 크다면, 음수를 반환
+// song1이 song2보다 작다면, 양수를 반환
+// 내림차순
+// song1이 song2보다 크다면, 양수를 반환
+// song1이 song2보다 작다면, 음수를 반환
+// 같다면 0을 반환
+
+
+// filter_array.sort(function (song1, song2) {
+//     const titleA = song1.title.toUpperCase(); // ignore upper and lowercase
+//     const titleB = song2.title.toUpperCase(); // ignore upper and lowercase
+//     if (titleA < titleB) {
+//         return -1;
+//     }
+//     if (titleA > titleB) {
+//         return 1;
+//     }
+//     if (titleA === titleB) {
+//         return 0;
+//     }
+// });

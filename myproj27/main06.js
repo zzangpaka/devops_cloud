9fca01375a8a35064820c7fd459562d1d1ad5c70
Jina Kim <fghjiurt@naver.com>
2021-12-22 12:39:55 +0900

    "곡명 / 단어수" 배열 구성(식 수정)

--- myproj27/main06.js
+++ myproj27/main06.js
@@ -7,12 +7,33 @@ console.log(``)
 // Array의 map 활용
 // 100줄에서 한 줄 출력의 예: `Dynamite / 1`
 
-const mapping_song = song_array.map(
-    (song) => (
-        { title: song.title, length: song.title.split(" ").length }
-    )
+// 1개의 song object를 문자열로 변환
+
+
+const title_array = song_array.map(
+    (song) => {
+        const word_count = song.title.trim().split(/\s+/).length;
+        return `${song.title} / ${word_count}`;
+    },
 );
 
-for (const song of mapping_song) {
-    console.log(`${song.title} / ${song.length}`);
+
+console.log(title_array)
+
+
+// 기존 song_array 배열에서 word_count라는 컬럼을 추가
+
+const new_song_array = song_array.map(
+    (song) => {
+        const word_count = song.title.trim().split(/\s+/).length;
+        return {
+            ...song,
+            word_count: word_count,
+        };
+    },
+);
+
+
+for (const new_song of new_song_array) {
+    console.log(new_song); // 기존 song + word_count 컬럼
 }

34470f212cc2fab98ab2b2f20b4c1bb14cee249c
Jina Kim <fghjiurt@naver.com>
2021-12-22 12:39:09 +0900

    좋아요수 top10 출력(식 수정)

--- myproj27/main03.js
+++ myproj27/main03.js
@@ -7,12 +7,13 @@ console.log(``)
 // Array의 sort 활용
 // 출력포맷 : `[좋아요수] 곡명 가수명`
 
-song_array.sort(function (song1, song2) {
-    return song2.like - song1.like;
-});
 
-const slice_name = song_array.slice(0, 10)
+const like_top10 = song_array
+    .sort(
+        (song1, song2) => song2.like - song1.like,
+    )
+    .slice(0, 10);
 
-for (const song of slice_name) {
-    console.log(`[${song.like}]`, song.title, song.artist);
+for (const [index, { like, title, artist }] of like_top10.entries()) {
+    console.log(`${index + 1} [${like}] ${title} by ${artist}`);
 }

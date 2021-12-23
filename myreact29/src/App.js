9a1e7cde1b2d8d55792d137c41f18a96bc26de2b
Jina Kim <fghjiurt@naver.com>
2021-12-23 14:29:07 +0900

    수업...

--- myreact29/src/App.js
+++ myreact29/src/App.js
@@ -1,9 +1,11 @@
 import Counter from "Counter";
+import MelonTop100 from "MelonTop100";
 
 function App() {
   return (
     <div>
       <h1>안녕 리액트</h1>
+      <MelonTop100 />
       <Counter />
       <Counter />
       <Counter />
--- myreact29/src/MelonTop100.jsx
+++ myreact29/src/MelonTop100.jsx
@@ -1,36 +1,10 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
+import initialSongList from "./data/melon_data.json";
+import "MelonTop100.css";
 
 // 전역 변수 : 바뀌지 않는 데이터
 //      - 초기값을 정의
-const initialSongList = [
-    {
-        "Unnamed: 0": 0,
-        "song_no": 32872978,
-        "title": "Dynamite",
-        "album": "Dynamite (DayTime Version)",
-        "artist": "방탄소년단",
-        "rank": 1,
-        "like": 354655
-    },
-    {
-        "Unnamed: 0": 1,
-        "song_no": 33077590,
-        "title": "VVS (Feat. JUSTHIS) (Prod. GroovyRoom)",
-        "album": "쇼미더머니 9 Episode 1",
-        "artist": "미란이",
-        "rank": 2,
-        "like": 40716
-    },
-    {
-        "Unnamed: 0": 2,
-        "song_no": 32998018,
-        "title": "힘든 건 사랑이 아니다",
-        "album": "힘든 건 사랑이 아니다",
-        "artist": "임창정",
-        "rank": 3,
-        "like": 74236
-    },
-];
 
 function MelonTop100() {
     const [songList, setSongList] = useState([]);
@@ -42,16 +16,13 @@ function MelonTop100() {
     return (
         <div>
             <h2>멜론 top 100</h2>
-            <button onClick={handleClick}>로딩</button>
-            <ul>
+            <Button onClick={handleClick}>로딩</Button>
+            <ul className="songList">
                 {
                     songList.map(song => {
-                        return <li>{song.title}</li>;
+                        return <li>{song.rank} {song.title} {song.artist}</li>;
                     })
                 }
-                <li>제목1</li>
-                <li>제목2</li>
-                <li>제목3</li>
             </ul>
         </div>
     );

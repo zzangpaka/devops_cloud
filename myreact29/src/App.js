4a5fcc7e6c35f35255cb619080acaa34e61cad3e
Jina Kim <fghjiurt@naver.com>
2021-12-23 13:20:29 +0900

    Counter 컴포넌트를 별도의 파일로 세팅

--- myreact29/src/App.js
+++ myreact29/src/App.js
@@ -1,33 +1,4 @@
-import { useState } from "react";
-
-function Counter() {
-  const [value, setValue] = useState(10);
-  const [color, setColor] = useState("red");
-
-  const handleClick = () => {
-    console.log(`clicked`);
-    setValue(value + 1);
-    setColor( value % 2 === 0 ? 'green' : 'red');
-  };
-  return ( 
-    <div
-      style={{
-        backgroundColor: color,
-        width: 100,
-        height: 100,
-        display: 'inline-block',
-        margin: 5,
-        borderRadius: 50,
-        textAlign: 'center',
-        userSelect: 'none',
-      }}
-      onClick={handleClick}
-    >
-      <span>{value}</span>
-    </div>
-  );
-}
-
+import Counter from "Counter";
 
 function App() {
   return (
@@ -39,6 +10,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;

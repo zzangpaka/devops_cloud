696a36d998147fd8f51026d53e5304a9de5d9813
JinA Kim <fghjiurt@daum.net>
2021-12-23 11:48:32 +0900

    오전수업

--- myreact29/src/App.js
+++ myreact29/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Counter() {
+  const [value, setValue] = useState(10);
+  const [color, setColor] = useState("red");
+
+  const handleClick = () => {
+    console.log(`clicked`);
+    setValue(value + 1);
+    setColor( value % 2 === 0 ? 'green' : 'red');
+  };
+  return ( 
+    <div style={{ 
+      backgroundColor: color,
+      width: 100,
+      height: 100,
+      display: 'inline-block',
+       
+      }}>
+      카운터 : {value}
+      <button onClick={handleClick}>증가</button>
     </div>
   );
 }
+
+
+function App() {
+  return (
+    <div>
+      <h1>안녕 리액트</h1>
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  );
+}
+
 
 export default App;

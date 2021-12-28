70025ac22e9f2f4ee2961f960eb74b8208fd7488
Jina Kim <fghjiurt@naver.com>
2021-12-28 11:00:26 +0900

    2단계 적용

--- myreact31/src/PageLotto.jsx
+++ myreact31/src/PageLotto.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 
 function PageLotto() {
-    return <h2>Lotto</h2>
+    const Numbers = useState([10, 11, 12, 13, 14, 15, 16, 17]);
+    return (
+        <> 
+        <h2>Lotto</h2>
+        {Numbers}
+        </>
+    );
 }
 
 

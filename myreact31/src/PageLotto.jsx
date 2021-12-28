fc293ff7d4cc9b356311d9ce1826802090116d0e
Jina Kim <fghjiurt@naver.com>
2021-12-28 17:27:15 +0900

    map 코드 수정(교수님)

--- myreact31/src/PageLotto.jsx
+++ myreact31/src/PageLotto.jsx
@@ -1,31 +1,34 @@
 import { useState } from "react";
 
-function makeLottoNumbers() {
-    const range = [...Array(45).keys()];
-    const randomInt = range
-    .sort(() => Math.random() - Math.random())
-    .map(( number ) => number +1)
-    .slice(0,7)
-    .sort((a, b) => a - b)
-    return randomInt;
-};
+const range = (size) => [...Array(size).keys()];
 
-function PageLotto() {
-    const [numbers, setNumbers] = useState([10, 11, 12, 13, 14, 15, 16, 17]);
-    const handleClick = () => {
-        setNumbers(makeLottoNumbers);
-    }
-    return (
-        <div> 
-            <h2>Lotto</h2>
-            <button onClick={handleClick}>예지</button> <br /> <br />
-                {numbers.map((number) => {return (<span 
-                style={{margin:'1rem'}} key={number}>
-                    {number} </span>);})}
-        </div>
-    );
+function makeLottoNumbers() {
+  const randomInt = range(45)
+    .sort(() => Math.random() - Math.random())
+    .map((number) => number + 1)
+    .slice(0, 7)
+    .sort((a, b) => a - b);
+  return randomInt;
 }
 
-
+function PageLotto() {
+  const [numbers, setNumbers] = useState([10, 11, 12, 13, 14, 15, 16, 17]);
+  const handleClick = () => {
+    setNumbers(makeLottoNumbers());
+  };
+  return (
+    <div>
+      <h2>Lotto</h2>
+      <button onClick={handleClick}>예지</button> <br /> <br />
+      {numbers.map((number) => {
+        return (
+          <span style={{ margin: "1rem" }} key={number}>
+            {number}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
 
 export default PageLotto;

5eb034b2b55c40a56645ae8e3bf1a6d4b260e992
Jina Kim <fghjiurt@naver.com>
2021-12-28 15:22:36 +0900

    1-6 적용

--- myreact31/src/PageLotto.jsx
+++ myreact31/src/PageLotto.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
+function makeLottoNumbers() {
+    const range = [...Array(45).keys()];
+    const randomInt = range
+    .sort(() => Math.random() - Math.random())
+    .map(( number ) => number +1)
+    .slice(0,7)
+    .sort((a, b) => a - b)
+    return randomInt;
+};
+
 function PageLotto() {
     const [numbers, setNumbers] = useState([10, 11, 12, 13, 14, 15, 16, 17]);
     const handleClick = () => {
-        console.log("clicked");
+        setNumbers(makeLottoNumbers);
     }
     return (
         <div> 

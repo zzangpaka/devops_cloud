dcbd9d490ef73877c03dd2884a54dd129b25491a
Jina Kim <fghjiurt@naver.com>
2021-12-28 13:06:57 +0900

    4단계 적용

--- myreact31/src/PageLotto.jsx
+++ myreact31/src/PageLotto.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 
 function PageLotto() {
     const [numbers, setNumbers] = useState([10, 11, 12, 13, 14, 15, 16, 17]);
+    const handleClick = () => {
+        console.log("clicked");
+    }
     return (
         <div> 
             <h2>Lotto</h2>
+            <button onClick={handleClick}>예지</button> <br /> <br />
                 {numbers.map((number) => {return (<span 
-                style={{margin:'1rem'}}>
+                style={{margin:'1rem'}} key={number}>
                     {number} </span>);})}
         </div>
     );

0e021cfc7060a11609db761d635b617a3038ea3c
Jina Kim <fghjiurt@naver.com>
2022-01-03 13:26:56 +0900

    스테이지 1-1

---
+++ myreact35/src/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Counter() {
+  const [value, setValue] = useState(0);
+  const [color, setColor] = useState("red");
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <div style={{ ...defaultStyle, backgroundColor: color }}>{value}</div>
+    </div>
+  );
+}
+
+const defaultStyle = {
+  width: "100px",
+  height: "100px",
+  borderRadius: "50px",
+  lineHeight: "100px",
+  textAlign: "center",
+  display: "inline-block",
+  fonstSize: "3rem",
+  userSelect: "none",
+};
+
+export default Counter;

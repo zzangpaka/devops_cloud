d49e18e051df18b476e2170d4fc575c5ddc954e0
Jina Kim <fghjiurt@naver.com>
2022-01-03 13:37:15 +0900

    스테이지 1-2

--- myreact35/src/Counter.jsx
+++ myreact35/src/Counter.jsx
@@ -4,10 +4,21 @@ function Counter() {
   const [value, setValue] = useState(0);
   const [color, setColor] = useState("red");
 
+  const handlePlus = () => {
+    setValue((prevValue) => prevValue + 1);
+  };
+
+  const handleMinus = () => {
+    setValue((prevValue) => prevValue - 1);
+  };
+
   return (
     <div>
       <h2>Counter</h2>
       <div style={{ ...defaultStyle, backgroundColor: color }}>{value}</div>
+      <hr />
+      <button onClick={handlePlus}>증가</button>
+      <button onClick={handleMinus}>감소</button>
     </div>
   );
 }
